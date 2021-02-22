76b1a09420cee163707b19bb5e73db1166ef8f1a
jagdish-singh-knoldus <76424088+jagdish-singh-knoldus@users.noreply.github.com>
2021-02-22 22:28:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello node!\n'
+  const msg = 'Hello Jagdish!\n'
   res.end(msg);
 });
 
 const server2 = http.createServer((req, res) => {
 	res.statusCode = 200;
-	const msg2 = 'Hello again from node!\n'
+	const msg2 = 'Hello again from Jagdish!\n'
 	res.end(msg2);
 });
 
